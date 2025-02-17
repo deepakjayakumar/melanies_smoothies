f2bd15e948d3dff8198aeaee3d18d85a35819405
deepakjayakumar <36305396+deepakjayakumar@users.noreply.github.com>
2025-02-17 17:48:42 +0530

    Update streamlit_app.py
    updating search on

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,9 @@ st.write(
 
 
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 
 
