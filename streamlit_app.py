97af187218385cbe218d78df19766794972e1988
deepakjayakumar <36305396+deepakjayakumar@users.noreply.github.com>
2025-02-17 17:24:43 +0530

    Update streamlit_app.py
    changing the respinse to json

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+
 
 cnx = st.connection("snowflake")
 
@@ -29,7 +28,8 @@ ingredients_list = st.multiselect('choose upto 5 ingredients', my_dataframe,max_
 order_name = st.text_input('Name of the Smoothie');
 st.write('The name of your smoothie is ' + order_name)
 time_to_insert = st.button('Submit',)
-
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
 if(ingredients_list):
     st.write(ingredients_list)
     st.text(ingredients_list)
